33d960a12488b9e13f3af2250cb444f4b8255f1f
Samir Schabel <schabel.samir@gmail.com>
2021-09-21 23:03:11 +0200

    add toggle button Bookmark

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
-const linkmain = document.querySelector(".link-main");
-const linksave = document.querySelector(".link-save");
-const linkadd = document.querySelector(".link-add");
-const linkprofile = document.querySelector(".link-profile");
-
-const pagehome = document.querySelector("#home");
-const pagebookmarked = document.querySelector("#bookmark-page");
-const pagecreate = document.querySelector("#create-page");
-const pageprofile = document.querySelector("#profile-page");
-
 const pages = document.querySelectorAll(".page")
 
 const links = document.querySelectorAll("footer a")
 
-links.forEach(link) => {
+links.forEach((link) => {
     link.addEventListener("click", () => {
         pages.forEach((page) => {
         page.classList.remove("current");
+        });
 
-const hrefAttribute = links.getAttribute("href");
-hrefAttribute.classList.add("current");
+const hrefAttribute = link.getAttribute("href");
+const nextPage = document.querySelector(hrefAttribute); 
+nextPage.classList.add("current");
 
         });
+});
+
+const button = document.querySelector(".bbm-bounce")
+
+button.addEventListener("click", () => {
+button.classList.toggle("bookmarkchange");
+});
