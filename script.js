b966b9bfa47bf0a5399fd652feffca859a88969e
Samir Schabel <schabel.samir@gmail.com>
2021-09-22 22:00:28 +0200

    Add Array

--- script.js
+++ script.js
@@ -28,3 +28,45 @@ const showMeAnswer = document.querySelector(".answer");
 showMeAnswer.addEventListener("click", () => {
 answer.classList.toggle("displayanswer");   
 });
+
+let question = {};
+
+const myArray = [];
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+        question = {
+        
+        newQuestion: form.elements['new-question'].value,
+        newAnswer: form.elements['new-answer'].value,
+        newTags: form.elements['new-tags'].value
+
+        }
+
+myArray.push(question);
+console.log(myArray);
+console.log(question);
+        event.preventDefault();
+        document.querySelector('form').reset();
+});
+
+
+
+/* Alex sachen 
+
+/ form stuff
+let input = {};
+
+const form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+  input = {
+    question: form.elements.newquestion.value,
+    answer: form.elements.newanswer.value,
+    questiontags: form.elements.newquestiontags.value
+  };
+  event.preventDefault();
+  form.reset();
+});
+
+*/
