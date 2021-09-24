6cc5bd287ab00561e34882dd5df1d33f31eab24d
Samir Schabel <schabel.samir@gmail.com>
2021-09-24 15:19:29 +0200

    change js

--- script.js
+++ script.js
@@ -1,72 +1,108 @@
-const pages = document.querySelectorAll(".page")
+const pages = document.querySelectorAll(".page");
 
-const links = document.querySelectorAll("footer a")
+const links = document.querySelectorAll("footer a");
 
 links.forEach((link) => {
-    link.addEventListener("click", () => {
-        pages.forEach((page) => {
-        page.classList.remove("current");
-        });
+  link.addEventListener("click", () => {
+    pages.forEach((page) => {
+      page.classList.remove("current");
+    });
 
-const hrefAttribute = link.getAttribute("href");
-const nextPage = document.querySelector(hrefAttribute); 
-nextPage.classList.add("current");
-
-        });
-});
-
-const button = document.querySelector(".bbm-bounce")
-
-button.addEventListener("click", () => {
-button.classList.toggle("bookmarkchange");
-});
-
-const answer = document.querySelector(".revealanswer");
-
-const showMeAnswer = document.querySelector(".answer");
-
-showMeAnswer.addEventListener("click", () => {
-answer.classList.toggle("displayanswer");   
+    const hrefAttribute = link.getAttribute("href");
+    const nextPage = document.querySelector(hrefAttribute);
+    nextPage.classList.add("current");
+  });
 });
 
 let question = {};
 
 const myArray = [];
 
-const form = document.querySelector('form');
-
-form.addEventListener('submit', (event) => {
-        question = {
-        
-        newQuestion: form.elements['new-question'].value,
-        newAnswer: form.elements['new-answer'].value,
-        newTags: form.elements['new-tags'].value
-
-        }
-
-myArray.push(question);
-console.log(myArray);
-console.log(question);
-        event.preventDefault();
-        document.querySelector('form').reset();
-});
-
-
-
-/* Alex sachen 
-
-/ form stuff
-let input = {};
-
 const form = document.querySelector("form");
+
 form.addEventListener("submit", (event) => {
-  input = {
-    question: form.elements.newquestion.value,
-    answer: form.elements.newanswer.value,
-    questiontags: form.elements.newquestiontags.value
+  question = {
+    newQuestion: form.elements["new-question"].value,
+    newAnswer: form.elements["new-answer"].value,
+    newTags: form.elements["new-tags"].value,
   };
+
+  myArray.push(question);
+  console.log(myArray);
+  console.log(question);
   event.preventDefault();
-  form.reset();
+  document.querySelector("form").reset();
 });
 
-*/
+const array = [
+  { question: "Are you sure?", answer: "No!", tags: "One Two Three" },
+  { question: "Are you really sure?", answer: "No!", tags: "One Two Three" },
+];
+
+const createQuestionsHtml = (questions) => {
+  let html = "";
+
+  array.forEach((question) => {
+    html =
+      html +
+      ` 
+        <div class="questcard">
+        
+        <h2>Question</h2>
+
+        <p class="question">Question: ${question.question}</p>
+
+<!------------Answer to Question-->
+<div class="revealanswer">
+<p >Answer: ${question.answer}</p>
+</div>
+
+<!--------Show Answer Button-->
+<button class="answer">
+    <h2>Show Answer</h2>
+</button>
+
+<!---------Tags-->
+
+<div class="tags">
+            <button><p id="tag1">Tag: ${question.tags}</p></button>
+            <button><p id="tag2">Tag: ${question.tags}</p></button>
+            <button><p id="tag3">Tag: ${question.tags}</p></button>
+            <button><p id="tag4">Tag: ${question.tags}</p></button>
+          </div>
+
+
+<!---------bookmark bouncing-->
+
+
+          <div class="bookm"></div>
+
+
+    </div>
+
+    `;
+  });
+  //console.log(html);
+  return html;
+};
+
+const renderQuestions = () => {
+  const questionsHtml = createQuestionsHtml(array);
+  const questionsContainer = document.querySelector("#home");
+  questionsContainer.innerHTML = questionsHtml;
+  const img = document.querySelector(".bookm");
+  console.log(img);
+  img.addEventListener("click", () => {
+    img.classList.toggle("bookmarkchange");
+  });
+
+  const answer = document.querySelector(".revealanswer");
+
+  const showMeAnswer = document.querySelector(".answer");
+
+  showMeAnswer.addEventListener("click", () => {
+    answer.classList.toggle("displayanswer");
+  });
+};
+
+renderQuestions();
